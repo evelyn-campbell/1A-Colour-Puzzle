0c83fdd3864356c5a861a6906ec3452a6e8ec867
Keya Tiwari <k6tiwari@uwaterloo.ca>
2021-11-24 00:22:57 -0500

    if statement under play_puzzle()

--- src/main.c
+++ src/main.c
@@ -31,8 +31,10 @@ bool in_colour_range (int mid_value, int uncertainty){
 
 }
 
-void play_puzzle(bool is_won)
-{
+void play_puzzle(bool is_won){ 
+    if (sequence_check == false) {
+        int sequence_num = 0;
+    } 
 }
 
 bool sequence_check (unsigned long item [3], int sequence_num){
