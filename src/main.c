04f3fc6e82de3fb23fd808ceeb470d76cf0813e5
Evelyn <e25campb@uwaterloo.ca>
2021-11-24 05:14:29 -0500

    bug fixes

--- src/main.c
+++ src/main.c
@@ -12,153 +12,6 @@
 
 #define TIME_RAND
 
-bool in_colour_range (unsigned long *item, int mid_value, int uncertainty){
-
-    if (item > (mid_value-uncertainty) && item < (mid_value+uncertainty)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-void play_puzzle(){ 
-    bool is_won = false;
-
-    while (!is_won){
-        int sequence_num = 0;
-        unsigned long *item = (unsigned long[3]){0,0,0};
-
-        // hardcoding values for demo purposes because measure_pulse does not work
-        // normally, a loop is called here that fills an array with red_value(), green_value(), blue_value(),
-        // and checks for correct sequences until an entire sequence is confirmed correct, after which it will
-        // declare is_won to be true
-        
-        while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-        item = &(unsigned long[3]){150,45,200};
-        sequence_num++;
-
-        if (sequence_check(item, sequence_num)){
-            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-            item = &(unsigned long[3]){220,125,50};
-            sequence_num++;
-            
-
-            if (sequence_check(item, sequence_num)){
-                while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-                item = &(unsigned long[3]){70,150,90};
-                sequence_num++;
-
-                if (sequence_check(item, sequence_num)){
-                    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-                    item = &(unsigned long[3]){85,185,85};
-                    sequence_num++;
-
-                    if (sequence_check(item, sequence_num)){
-                        while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-                        item = &(unsigned long[3]){205,80,170};
-                        sequence_num++;
-
-                        if (sequence_check(item, sequence_num)){
-                            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
-                            is_won = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!is_won){
-            SerialPuts("Sequence check failed");
-        }
-
-        free (item);
-    }
-    
-    SerialPuts("Puzzle Solved!");
-    
-}
-
-bool sequence_check (unsigned long *item [3], int sequence_num){
-    size_t rgb = 3;
-    unsigned long ans1 [rgb];   // purple
-    unsigned long ans2 [rgb];   // orange
-    unsigned long ans3 [rgb];   // blue
-    unsigned long ans4 [rgb];   // green
-    unsigned long ans5 [rgb];   // pink
-
-    // purple
-    ans1[0] = 150;
-    ans1[1] = 45;
-    ans1[2] = 200;
-
-    // orange
-    ans2[0] = 220;
-    ans2[1] = 125;
-    ans2[2] = 50;
-
-    // blue
-    ans3[0] = 70;
-    ans3[1] = 150;
-    ans3[2] = 190;
-
-    // green
-    ans4[0] = 85;
-    ans4[1] = 185;
-    ans4[2] = 85;
-
-    // pink
-    ans5[0] = 205;
-    ans5[1] = 80;
-    ans5[2] = 170;
-
-    if (sequence_num == 1){
-        // compare rgb values
-        if (in_colour_range(item[0], ans1[0], 60) && in_colour_range(item[1], ans1[1], 45) && in_colour_range(item[2], ans1[2], 75)){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else if (sequence_num == 2){
-        if (in_colour_range(item[0], ans2[0], 35) && in_colour_range(item[1], ans2[1], 50) && in_colour_range(item[2], ans2[2], 50)){
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-    else if (sequence_num == 3){
-         if (in_colour_range(item[0], ans3[0], 70) && in_colour_range(item[1], ans3[1], 60) && in_colour_range(item[2], ans3[2], 60)){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else if (sequence_num == 4){
-        if (in_colour_range(item[0], ans4[0], 85) && in_colour_range(item[1], ans4[1], 70) && in_colour_range(item[2], ans4[2], 85)){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else if (sequence_num == 5){
-        if (in_colour_range(item[0], ans5[0], 50) && in_colour_range(item[1], ans5[1], 80) && in_colour_range(item[2], ans5[2], 85)){
-            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    return false;
-}
-
 // measure frequency  - change to frequency alg when possible
 unsigned long measure_frequency(){
     /* uint16_t value1 = 0;
@@ -233,8 +86,154 @@ unsigned long green_value() {
     return green;
 }
 
+bool in_colour_range (unsigned long item, int mid_value, int uncertainty){
 
-// collect red colour
+    int item_value = item;
+
+    if (item_value > (mid_value-uncertainty) && item_value < (mid_value+uncertainty)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool sequence_check (unsigned long *item, int sequence_num){
+    size_t rgb = 3;
+    unsigned long ans1 [rgb];   // purple
+    unsigned long ans2 [rgb];   // orange
+    unsigned long ans3 [rgb];   // blue
+    unsigned long ans4 [rgb];   // green
+    unsigned long ans5 [rgb];   // pink
+
+    // purple
+    ans1[0] = 150;
+    ans1[1] = 45;
+    ans1[2] = 200;
+
+    // orange
+    ans2[0] = 220;
+    ans2[1] = 125;
+    ans2[2] = 50;
+
+    // blue
+    ans3[0] = 70;
+    ans3[1] = 150;
+    ans3[2] = 190;
+
+    // green
+    ans4[0] = 85;
+    ans4[1] = 185;
+    ans4[2] = 85;
+
+    // pink
+    ans5[0] = 205;
+    ans5[1] = 80;
+    ans5[2] = 170;
+
+    if (sequence_num == 1){
+        // compare rgb values
+        if (in_colour_range(*(item), ans1[0], 60) && in_colour_range(*(item+1), ans1[1], 45) && in_colour_range(*(item+2), ans1[2], 75)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (sequence_num == 2){
+        if (in_colour_range(*(item), ans2[0], 35) && in_colour_range(*(item+1), ans2[1], 50) && in_colour_range(*(item+2), ans2[2], 50)){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    else if (sequence_num == 3){
+         if (in_colour_range(*(item), ans3[0], 70) && in_colour_range(*(item+1), ans3[1], 60) && in_colour_range(*(item+2), ans3[2], 60)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (sequence_num == 4){
+        if (in_colour_range(*(item), ans4[0], 85) && in_colour_range(*(item+1), ans4[1], 70) && in_colour_range(*(item+2), ans4[2], 85)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (sequence_num == 5){
+        if (in_colour_range(*(item), ans5[0], 50) && in_colour_range(*(item+1), ans5[1], 80) && in_colour_range(*(item+2), ans5[2], 85)){
+            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    return false;
+}
+
+void play_puzzle(){ 
+    bool is_won = false;
+
+    while (!is_won){
+        int sequence_num = 0;
+        unsigned long *item;
+
+        // hardcoding values for demo purposes because measure_pulse does not work
+        // normally, a loop is called here that fills an array with red_value(), green_value(), blue_value(),
+        // and checks for correct sequences until an entire sequence is confirmed correct, after which it will
+        // declare is_won to be true
+        
+        while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+        item = (unsigned long[3]){150,45,200};
+        sequence_num++;
+
+        if (sequence_check(item, sequence_num)){
+            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+            item = (unsigned long[3]){220,125,50};
+            sequence_num++;
+            
+
+            if (sequence_check(item, sequence_num)){
+                while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+                item = (unsigned long[3]){70,150,90};
+                sequence_num++;
+
+                if (sequence_check(item, sequence_num)){
+                    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+                    item = (unsigned long[3]){85,185,85};
+                    sequence_num++;
+
+                    if (sequence_check(item, sequence_num)){
+                        while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+                        item = (unsigned long[3]){205,80,170};
+                        sequence_num++;
+
+                        if (sequence_check(item, sequence_num)){
+                            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // pending button press...
+                            is_won = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!is_won){
+            SerialPuts("Sequence check failed");
+        }
+
+        free (item);
+    }
+    
+    SerialPuts("Puzzle Solved!");   
+}
+
 
 int main(void)
 {       
